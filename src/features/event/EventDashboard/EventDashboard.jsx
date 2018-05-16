61139c260df65d942c8c0bd4aefb193ca3cdb465
Suthicha Poonakaow <suthicha@outlook.com>
2018-05-16 11:55:21 +0700

    added EventForm

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Button } from "semantic-ui-react";
+import cuid from "cuid";
 import EventList from "../EventList/EventList";
 import EventForm from "../EventForm/EventForm";
 
@@ -68,6 +69,16 @@ class EventDashboard extends Component {
     this.setState({ isOpen: false });
   };
 
+  handleCreateEvent = newEvent => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = "/assets/user.png";
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    });
+  };
+
   render() {
     return (
       <Grid>
@@ -80,8 +91,9 @@ class EventDashboard extends Component {
             positive
             content="Create Event"
           />
-          {this.state.isOpen && 
-          <EventForm handleCancel={this.handleFormCancel} />}
+          {this.state.isOpen && (
+            <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleFormCancel} />
+          )}
         </Grid.Column>
       </Grid>
     );
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -28,7 +28,7 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-            {event.attendees.map(attendee => (
+            {event.attendee && event.attendees.map(attendee => (
               <EventListAttendee key={attendee.id} attendee={attendee} />
             ))}
           </List>
