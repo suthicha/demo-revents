10f3c06e327c08e8232d8f129ed6fd956b5e703d
Suthicha Poonakaow <suthicha@outlook.com>
2018-05-17 17:37:52 +0700

    Fix datetime

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,9 +1,12 @@
 import {
     combineReducers
 } from 'redux';
+import { reducer as FormReducer  } from 'redux-form';
 import eventReducer from '../../features/event/eventReducer';
 
+
 const rootReducer = combineReducers({
+    form: FormReducer,
     events: eventReducer
 });
 
