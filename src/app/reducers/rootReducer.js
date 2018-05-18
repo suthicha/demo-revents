70755cf947cebb51e370a7e181568283beb89cf4
Suthicha Poonakaow <suthicha@outlook.com>
2018-05-18 08:06:30 +0700

    Added modals

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,11 +3,12 @@ import {
 } from 'redux';
 import { reducer as FormReducer  } from 'redux-form';
 import eventReducer from '../../features/event/eventReducer';
-
+import modalReducer from '../../features/modals/modalReducer'
 
 const rootReducer = combineReducers({
     form: FormReducer,
-    events: eventReducer
+    events: eventReducer,
+    modals: modalReducer
 });
 
 export default rootReducer;
